e4fc428c408d78e02542d3c389448f0c4c86825a
Jefri HJS <jefrihjs@gmail.com>
2026-04-06 08:37:49 +0700

    Fix: Menambahkan Content Security Policy (CSP) unsafe-eval untuk Alpine.js di Intranet

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+        
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self' http: https: data: blob: 'unsafe-inline' 'unsafe-eval';">
+        
         <title>{{ config('app.name', 'SiCERIA - Sistem Pelaporan ASN') }}</title>
 
        
